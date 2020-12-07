ff984a3bdc1548ddd2130157655aebd181bfbbe1
coolcheeto <68211116+coolcheeto@users.noreply.github.com>
2020-12-07 00:55:11 -0600

    Add files via upload

--- cmdloop.h
+++ cmdloop.h
@@ -7,36 +7,37 @@
 
 using namespace std;
 
-string splitter(string playerInput, int wordReturnNum) // I sniffed a lot of paint before writing this function. I'm not sure what it does.
+struct playerInputContainer {
+	string pi[9]; // maximum of 10 words. if you want more for some reason, just increase the magic number.
+};
+
+playerInputContainer splitter(string playerInput) // I sniffed a lot of paint before writing this function. I'm not sure what it does.
 {
+	playerInputContainer pic;
 	string line = playerInput;
-	string arr[4];
 	int i = 0;
 	stringstream ssin(line);
-	while (ssin.good() && i < 4)
+	while (ssin.good() && i < 9)
 	{
-		ssin >> arr[i];
+		ssin >> pic.pi[i];
 		++i;
 	}
 
-	return arr[wordReturnNum];
+	return pic;
 }
 
 void cmdloop() // This function serves as an interpreter for player commands. It reads the player command and runs the appropriate function.
 {
 	string playerInput; // Pre-split player input.
-	string pi[3]; // Short for player input. Each place in the variable contains one of the words in the input. i.e. '0' contains the first word, '1' contains the second etc.
+	playerInputContainer pic;
 	while (true)
 	{
 		cout<<"player@playercomputer> ";
 		getline(std::cin, playerInput); // Get player input and put it in the string variable 'playerInput'
-		
-		pi[0] = splitter(playerInput, 0); // Splits the string we get from playerInput and splits it into multiple strings. For example, 'hello world' turns into 'hello' and 'world'
-		pi[1] = splitter(playerInput, 1);
-		pi[2] = splitter(playerInput, 2);
-		pi[3] = splitter(playerInput, 3);
 	
-		if (pi[0] == "printhelloisabel") // Simple example command.
+		pic = splitter(playerInput);
+		
+		if (pic.pi[0] == "printhelloisabel") // Simple example command.
 		{
 			cout<< "Hello, Isabel!\n";
 		} else
