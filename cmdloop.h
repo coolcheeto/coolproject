804c017e8dc795339dd16a4b8a6befec6ae4c492
coolcheeto <68211116+coolcheeto@users.noreply.github.com>
2020-12-06 23:23:12 -0800

    Add files via upload

--- cmdloop.h
+++ cmdloop.h
@@ -7,37 +7,25 @@
 
 using namespace std;
 
-struct playerInputContainer {
-	string pi[9]; // maximum of 10 words. if you want more for some reason, just increase the magic number.
-};
-
-playerInputContainer splitter(string playerInput) // I sniffed a lot of paint before writing this function. I'm not sure what it does.
-{
-	playerInputContainer pic;
-	string line = playerInput;
-	int i = 0;
-	stringstream ssin(line);
-	while (ssin.good() && i < 9)
-	{
-		ssin >> pic.pi[i];
-		++i;
-	}
-
-	return pic;
-}
-
 void cmdloop() // This function serves as an interpreter for player commands. It reads the player command and runs the appropriate function.
 {
+	int i = 0;
 	string playerInput; // Pre-split player input.
-	playerInputContainer pic;
+	string pi[9];
 	while (true)
 	{
 		cout<<"player@playercomputer> ";
 		getline(std::cin, playerInput); // Get player input and put it in the string variable 'playerInput'
-	
-		pic = splitter(playerInput);
 		
-		if (pic.pi[0] == "printhelloisabel") // Simple example command.
+		i = 0;	
+		stringstream ssin(playerInput);
+		while (ssin.good() && i < 9)
+		{
+			ssin >> pi[i];
+			++i;
+		}
+		
+		if (pi[0] == "printhelloisabel") // Simple example command.
 		{
 			cout<< "Hello, Isabel!\n";
 		} else
