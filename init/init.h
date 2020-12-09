7a0274d0786c2cd30cda8a36ebb8666e241293c7
coolcheeto <68211116+coolcheeto@users.noreply.github.com>
2020-12-09 02:23:26 -0600

    Add files via upload

---
+++ init/init.h
@@ -0,0 +1,33 @@
+#include <vector>
+
+#ifndef INIT_H
+#define INIT_H
+
+void placeInsideFolder(int parentdir, int childdir)
+{
+	folders[parentdir].children.push_back(childdir);
+	folders[childdir].parent = parentdir;
+	return;
+}
+
+void init()
+{
+	/* FOLDERS */
+	folders.push_back(Folder()); // ID 0
+	folders[0].name = "/";
+
+	folders.push_back(Folder()); // ID 1
+	folders[1].name = "Documents";
+
+	folders.push_back(Folder()); // ID 2
+	folders[2].name = "Videos";
+
+	folders.push_back(Folder()); // ID 3
+	folders[3].name = "Pictures";
+	
+	placeInsideFolder(0, 1);
+	placeInsideFolder(0, 2);
+	placeInsideFolder(0, 3);
+}
+
+#endif
