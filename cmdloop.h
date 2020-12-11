2e0f075508a1a0d1aaabfc0b49ef78e72a910b19
Isabel <rosiejo2mars@gmail.com>
2020-12-10 23:45:58 -0800

    Added some comments

--- cmdloop.h
+++ cmdloop.h
@@ -15,15 +15,16 @@ vector<Folder> folders;
 void cmdloop() // This function serves as an interpreter for player commands. It reads the player command and runs the appropriate function.
 {
 	int i = 0;
-	string playerInput; // Pre-split player input.
+	string playerInput; // Unsplit player input.
 	string pi[9];
 	while (true)
 	{
 		cout<<"player@playercomputer:"<<folders[workingdir].name<<"> ";
-	
+		
+		// Player Input Processing
 		getline(std::cin, playerInput); // Get player input and put it in the string variable 'playerInput'
 		
-		i = 0;	
+		i = 0;
 		stringstream ssin(playerInput);
 		while (ssin.good() && i < 9)
 		{
@@ -31,6 +32,7 @@ void cmdloop() // This function serves as an interpreter for player commands. It
 			++i;
 		}
 		
+		// Commands
 		if (pi[0] == "cd" && pi[1] != "") 
 		{
 			cd(pi[1]);
@@ -38,18 +40,16 @@ void cmdloop() // This function serves as an interpreter for player commands. It
 		else if (playerInput == "ls")
 		{
 			ls();
-		} else if (playerInput == "pwd")
-		{
-			cout<<workingdir;
-		} else
+		}else
 		{
 			cout<<"Command not found.\n"; // If the player input doesn't corrospond to any registered commands.
 		}
-	
-	for (int i = 0; i <= 8; i++)
-	{
-		pi[i] = "";
-	}
+		
+		// Resetting the player input
+		for (int i = 0; i <= 8; i++)
+		{
+			pi[i] = "";
+		}
 	}
 	return;
 }
