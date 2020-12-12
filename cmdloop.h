6286494eb236582bac4e95e96b58a7a480c222b2
Isabel <rosiejo2mars@gmail.com>
2020-12-11 20:45:49 -0800

    idk did shit lol added split function commented and shit

--- cmdloop.h
+++ cmdloop.h
@@ -12,37 +12,47 @@ using namespace std;
 
 vector<Folder> folders;
 
+vector<string> split(const string& s, char delimiter)
+{
+	vector<string> tokens;
+	string token;
+	istringstream tokenStream(s);
+	while (getline(tokenStream, token, delimiter))
+	{
+		tokens.push_back(token);
+	}
+	return tokens;
+}
+
 void cmdloop() // This function serves as an interpreter for player commands. It reads the player command and runs the appropriate function.
 {
 	int i = 0;
 	string playerInput; // Unsplit player input.
-	string pi[9];
+	vector<string> pi;
 	while (true)
 	{
 		cout<<"player@playercomputer:"<<folders[workingdir].name<<"> ";
 		
 		// Player Input Processing
-		getline(std::cin, playerInput); // Get player input and put it in the string variable 'playerInput'
+		getline(cin, playerInput); // Get player input and put it in the string variable 'playerInput'
 		
-		i = 0;
-		stringstream ssin(playerInput);
-		while (ssin.good() && i < 9)
-		{
-			ssin >> pi[i];
-			++i;
-		}
+		pi = split(playerInput, ' ');
 		
 		// Commands
-		if (pi[0] == "cd" && pi[1] != "") 
+		if (playerInput != "")
 		{
-			cd(pi[1]);
-		}
-		else if (playerInput == "ls")
-		{
-			ls();
-		}else
-		{
-			cout<<"Command not found.\n"; // If the player input doesn't corrospond to any registered commands.
+			if (pi[0] == "cd" && pi.size() > 1)
+			{
+				cd(pi[1]);
+			}
+			else if (playerInput == "ls")
+			{
+				ls();
+			}
+			else
+			{
+				cout << "Command not found.\n"; // If the player input doesn't corrospond to any registered commands.
+			}
 		}
 		
 		// Resetting the player input
